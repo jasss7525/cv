f1515391a699bb7a051aa0be189456570d86544a
jaspreet <jasss7525.upwork@gmail.com>
2022-10-23 23:23:15 +0530

    fixes for server file

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/cv'));
-
+app.use(express.static(__dirname + '/dist/'));
 app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/index.html'));});
+// app.listen(process.env.PORT || 8080);
 
-  if(req.url === '/') {
-    res.sendFile(path.join(__dirname,'/dist/cv/index.html'));
-  }
-  else {
-    res.sendFile(path.join(__dirname, '/dist/cv' + req.url))
-  }
-
-});
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
